b1abcf4a8e811a8d9a194a513b68ad5f3c75ba91
Jyotsna2029 <chauhanjyotsna049@gmail.com>
2026-08-16 18:01:52 +0530

    Add files via upload

---
+++ src/textSummarizer/entity/__init__.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from dataclasses import dataclass
+
+# 2. Define Entity Dataclass
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
---
+++ template.py
@@ -0,0 +1,58 @@
+import os
+from pathlib import Path
+import logging
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s]: %(message)s:'
+)
+
+# Project name
+project_name = "textSummarizer"
+
+# Project files and directories
+list_of_files = [
+     ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "test.py"
+]
+
+# Create files and directories
+for filepath in list_of_files:
+    filepath = Path(filepath)
+
+    filedir = filepath.parent
+    filename = filepath.name
+
+    # Create directory if needed
+    if str(filedir) != ".":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(
+            f"Creating directory: {filedir} for the file {filename}"
+        )
+
+    # Create file if it doesn't exist or is empty
+    if not filepath.exists() or filepath.stat().st_size == 0:
+        filepath.touch()
+        logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} already exists")
