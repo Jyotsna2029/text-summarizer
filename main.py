ea12d492913055acaf0645ffe8fd47bb2efb508e
Jyotsna2029 <chauhanjyotsna049@gmail.com>
2026-08-16 20:23:02 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
+from textSummarizer.pipeline.stage_01_data_ingestion import (
+    DataIngestionTrainingPipeline,
+)
+from textSummarizer.pipeline.stage_02_data_validation import (
+    DataValidationTrainingPipeline,
+)
 
+# ----------------- Stage 01: Data Ingestion -----------------
 STAGE_NAME = "Data Ingestion stage"
 try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    data_ingestion = DataIngestionTrainingPipeline()
-    data_ingestion.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+  logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+  data_ingestion = DataIngestionTrainingPipeline()
+  data_ingestion.main()
+  logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise e
+  logger.exception(e)
+  raise e
 
 
-
-
+# ----------------- Stage 02: Data Validation -----------------
+STAGE_NAME = "Data Validation stage"
+try:
+  logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+  data_validation = DataValidationTrainingPipeline()
+  data_validation.main()
+  logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+  logger.exception(e)
+  raise e
